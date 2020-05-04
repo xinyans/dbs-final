47670b23791134a40d1c354c3f3f6cfd5a53a780
tn20000 <liangth.frank2015@outlook.com>
2020-05-03 20:40:50 -0400

    Fixed some syntatic errors

--- code/database.py
+++ code/database.py
@@ -1,12 +1,16 @@
-import psycorg2
-import psycorg2.extras
+import psycopg2
+import psycopg2.extras
+
 class Vehicles_data:
 	def __init__(self, connection_string):
-        self.conn = psycopg2.connect(connection_string)
-    def __init__(self,volume):
-    	cursor = self.conn.cursor
-    	query = "SELECT municipality,year FROM Location_volume WHERE sum(count)>=%d \
-    	group by municipality,year"
-    	cursor.execute(query,(volume,))
-    	records=cursor.fetchall()
-    	return records
+		self.conn = psycopg2.connect(connection_string)
+
+	def listTrafficVolume(self,volume):
+		cursor = self.conn.cursor()
+		query = """
+		SELECT municipality,year FROM Location_volume 
+		GROUP BY municipality,year HAVING SUM(volume_count) >= %s
+		"""
+		cursor.execute(query,(volume,))
+		records=cursor.fetchall()
+		return records
