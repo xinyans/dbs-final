8619152521fe3afb2fe2754e9108fc365ee2451e
xinyans <sunx15@rpi.edu>
2020-05-05 20:55:31 -0400

    Another query

--- code/database.py
+++ code/database.py
@@ -38,8 +38,11 @@ class Vehicles_data:
 		records=cursor.fetchall()
 		return records
 
-	# User selects from the following: ramp, bridge, railroad crossing, and one-way.
+	# User selects from the following: ramp, bridge, railroad crossing, and one-way. Any other input is not allowed.
 	# Function returns how these affect crash rate
+	# Return structure:	crash_total	volume_total	percentage	has_structure
+	# 					12			100				12			Y
+	# 					35			500				7			null
 	def structureCrashRelation(self, structure_name):
 		cursor = self.conn.cursor()
 		query = """
@@ -58,10 +61,27 @@ class Vehicles_data:
 		records = cursor.fetchall()
 		return records
 
-	# User inputs a municipality name, case insensitive. If this municipality is not found, return empty.
+	# User inputs a municipality and county name pair, case insensitive. If this pair is not found, return empty.
 	# Else return Crashes, Volume and crash rate of every year.
-	def crashRateMunicipality(self, municipality_name):
-		pass
+	# Return structure:	year	vol		crash_count	percentage
+	# 					2014	9913	108			1.08
+	# 					2015	12455	82			0.66
+	# 					2016	11676	96			0.82
+	def crashRateMunicipality(self, municipality_name, county_name):
+		cursor = self.conn.cursor()
+		query = """
+				SELECT a.year, a.vol, b.crash_count, CAST(b.crash_count AS FLOAT)/CAST(a.vol AS FLOAT)*100 AS percentage FROM 
+					(SELECT year, municipality, county, SUM(volume_count) AS vol FROM
+					Location_volume GROUP BY (year, municipality, county)) a
+				JOIN
+					(SELECT year, municipality, county, COUNT(incident_date) AS crash_count FROM Car_crash GROUP BY (year, municipality, county)) b
+				ON a.year=b.year and LOWER(a.municipality)=LOWER(b.municipality) and LOWER(a.county)=LOWER(b.county)
+				WHERE a.municipality=%s and a.county=%s
+				GROUP BY (a.year, a.vol, b.crash_count);
+				"""
+		cursor.execute(query, (municipality_name, county_name))
+		records = cursor.fetchall()
+		return records
 
 
 
